39247c84dfbc30b432681ca40cd58c5563657216
RafaelJCamara <rafaelcamarac@gmail.com>
2022-11-30 00:08:02 +0000

    Add basic Catalog API structure.

---
+++ src/PlayCatalog.Service/PlayCatalog.Service/Controllers/ItemsController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PlayCatalog.Service.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PlayCatalog.Service.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        private readonly List<ItemDto> items = new() 
+        {
+            new ItemDto(Guid.NewGuid(), "Potion", "Restores a small amount of HP", 5, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Antidote", "Cures poison", 7, DateTimeOffset.UtcNow),
+            new ItemDto(Guid.NewGuid(), "Bronze sword", "Deals a small amount of damage", 20, DateTimeOffset.UtcNow)
+        };
+
+        [HttpGet]
+        public IEnumerable<ItemDto> Get()
+        {
+            return items;
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<ItemDto> GetById(Guid id)
+        {
+            var existingItem = items
+                                    .Where(item => item.Id == id)
+                                    .SingleOrDefault();
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+            return existingItem;
+        }
+
+        [HttpPost]
+        public ActionResult<ItemDto> Post(CreateItemDto createItemDto)
+        {
+            var item = new ItemDto(Guid.NewGuid(), createItemDto.Name, createItemDto.Description, createItemDto.Price, DateTimeOffset.UtcNow);
+            items.Add(item);
+
+            /*
+                With CreatedAtAction, in the headers field, we will have a location attribute that will give us the path/url to find the item
+             */
+            return CreatedAtAction(nameof(GetById), new { id = item.Id }, item);
+        }
+
+        [HttpPut]
+        public IActionResult Put(Guid id, UpdateItemDto updateItemDto)
+        {
+            var existingItem = items.Where(item => item.Id == id).SingleOrDefault();
+
+            if (existingItem == null)
+            {
+                return NotFound();
+            }
+
+            var updatedItem = existingItem with
+            {
+                Name = updateItemDto.Name,
+                Description = updateItemDto.Description,
+                Price = updateItemDto.Price
+            };
+
+            var index = items.FindIndex(existingItem => existingItem.Id == id);
+            items[index] = updatedItem;
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            var index = items.FindIndex(existingItem => existingItem.Id == id);
+
+            if (index < 0)
+            {
+                return NotFound();
+            }
+
+            items.RemoveAt(index);
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ src/PlayCatalog.Service/PlayCatalog.Service/Dtos/Dto.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlayCatalog.Service.Dtos
+{
+    /*
+        DTO retrieved for GET reqeests
+     */
+    /*
+        We use decimal here because we care about precision and are performing money based operations
+        Excelent resource to understance the impact of choosing decimal over double (for example)
+        (https://exceptionnotfound.net/decimal-vs-double-and-other-tips-about-number-types-in-net/)
+     */
+    /*
+        Example of DateTimeOffset: 5/1/2008 8:06:32 AM -07:00
+     */
+    public record ItemDto(Guid Id, string Name, string Description, decimal Price, DateTimeOffset CreatedDate);
+
+    /*
+        DTO used to create an item
+     */
+    public record CreateItemDto([Required] string Name, string Description, [Range(0, 100)] decimal Price);
+
+    /*
+        DTO used to update an item
+     */
+    public record UpdateItemDto([Required] string Name, string Description, [Range(0, 100)] decimal Price);
+}
